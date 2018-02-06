86199388ce12250d71d7e8451bc96109afdb3b60
evgensobolev <evgen_sobolev@mail.ru>
2018-02-06 10:27:15 +0300

    Fix one action - one function

--- pprint_json.py
+++ pprint_json.py
@@ -1,32 +1,26 @@
 import json
 import sys
-import os
 
 
 def load_data(filepath):
-    if os.path.isfile(filepath):
-        input_file = open(filepath, 'r')
-        return input_file
-    else:
-        return 0
-
-
-def pretty_print_json(input_file):
     try:
-        json_data = json.load(input_file)
+        json_data = json.load(filepath)
     except ValueError:
         print("This is not JSON")
     else:
-        print(json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4))
+        return json_data
+
+
+def pretty_print_json(json_data):
+    print(json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
 if __name__ == '__main__':
-    if sys.argv.__len__() == 1:
+    try:
+        with open(sys.argv[1], 'r') as input_file:
+            json_data = load_data(input_file)
+            pretty_print_json(json_data)
+    except IndexError:
         print("You did not enter a file name")
-    else:
-        json_file = load_data(sys.argv[1])
-        if json_file == 0:
-            print("This file does not exist")
-        else:
-            pretty_print_json(json_file)
-            json_file.close()
+    except IOError:
+        print("Such file does not exist")
