5cb0c77dc96382068a63fd50899de9288cca4b1f
evgensobolev <evgen_sobolev@mail.ru>
2018-02-07 12:00:59 +0300

    Delete else: return

--- pprint_json.py
+++ pprint_json.py
@@ -4,11 +4,9 @@ import sys
 
 def load_data(filepath):
     try:
-        json_data = json.load(filepath)
+        return json.load(filepath)
     except ValueError:
         print("This is not JSON")
-    else:
-        return json_data
 
 
 def pretty_print_json(json_data):
@@ -19,7 +17,8 @@ if __name__ == '__main__':
     try:
         with open(sys.argv[1], 'r') as input_file:
             json_data = load_data(input_file)
-            pretty_print_json(json_data)
+            if json_data != None:
+                pretty_print_json(json_data)
     except IndexError:
         print("You did not enter a file name")
     except IOError:
